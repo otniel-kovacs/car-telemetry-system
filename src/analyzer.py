350ee26f003cc858440132547b5715f9a0a82e9b
otniel-kovacs <scottmall80@gmail.com>
2026-07-10 11:59:33 +0300

    Remove warning emoji

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,9 +1,9 @@
 def check_speed(speed):
 
     if speed > 130:
-        print("⚠️ Depasire viteza!")
+        print(" Depasire viteza!")
 
 def check_rpm(rpm):
 
     if rpm > 4500:
-        print("⚠️ RPM ridicat!")
+        print(" RPM ridicat!")
